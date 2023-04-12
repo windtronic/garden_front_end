1d4aadfa6e7b267cdebadd6f2446ad4b352d9bc2
Toni <windtronic89@gmail.com>
2023-04-12 15:44:30 -0600

    more css

--- garden_front_end/src/components/GardenLayout.jsx
+++ garden_front_end/src/components/GardenLayout.jsx
@@ -93,31 +93,31 @@ return (
     </div>
     <div className="container">
     <div className="grid-container">
-      <div className="item">1</div>
-      <div className="item">2</div>
-      <div className="item">3</div>
-      <div className="item">4</div>
-      <div className="item">5</div>
-      <div className="item">6</div>
-      <div className="item">7</div>
-      <div className="item">8</div>
-      <div className="item">9</div>
-      <div className="item">10</div>
-      <div className="item">11</div>
-      <div className="item">12</div>
-      <div className="item">13</div>
-      <div className="item">14</div>
-      <div className="item">15</div>
-      <div className="item">16</div>
-      <div className="item">17</div>
-      <div className="item">18</div>
-      <div className="item">19</div>
-      <div className="item">20</div>
-      <div className="item">21</div>
-      <div className="item">22</div>
-      <div className="item">23</div>
-      <div className="item">24</div>
-      <div className="item">25</div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      
     </div>
   </div>
   </div>
