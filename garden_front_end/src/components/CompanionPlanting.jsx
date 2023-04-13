bdb7071a9424a48c6c3dfa0397ea3f9f5458d0a1
Toni <windtronic89@gmail.com>
2023-04-13 14:52:23 -0600

    css for companion planting

--- garden_front_end/src/components/CompanionPlanting.jsx
+++ garden_front_end/src/components/CompanionPlanting.jsx
@@ -1,9 +1,263 @@
 export default function CompanionPlanting() {
+  return (
+    <div class="chart">
+      <div class="row">
+        <div class="cell">
+          <p>Arugula:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can be grown with lettuce and other leafy greens, which can provide
+            shade and help retain moisture. It can also be grown with herbs such
+            as thyme and oregano, which repel pests such as flea beetles and
+            aphids.
+          </p>
+        </div>
+      </div>
 
+      <div class="row">
+        <div class="cell">
+          <p>Beets:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can be grown with garlic and onions, which repel pests such as leaf
+            miners and aphids. They can also be grown with bush beans, which fix
+            nitrogen in the soil.
+          </p>
+        </div>
+      </div>
 
-    return (
-        <div>
-<p>Arugula: Can be grown with lettuce and other leafy greens, which can provide shade and help retain moisture. It can also be grown with herbs such as thyme and oregano, which repel pests such as flea beetles and aphids.</p>
+      <div class="row">
+        <div class="cell">
+          <p>Borage:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Attracts pollinators such as bees and beneficial insects such as
+            hoverflies and parasitic wasps. It also accumulates potassium,
+            calcium, and other nutrients in the soil.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="cell">
+          <p>Basil:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can be grown with tomatoes, peppers, and eggplants, which improve
+            their flavor and repel pests such as spider mites and aphids. It can
+            also be grown with chamomile and thyme, which attract beneficial
+            insects such as bees and parasitic wasps.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="cell">
+          <p>Carrots:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can be grown with onions, leeks, and other alliums, which repel
+            pests such as carrot flies and aphids. They can also be grown with
+            herbs such as dill and parsley, which attract beneficial insects
+            such as wasps and hoverflies.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="cell">
+          <p>Cilantro:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can be grown with dill and parsley, which attract beneficial insects
+            such as wasps and hoverflies. They can also be grown with tomatoes,
+            which improve their flavor.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="cell">
+          <p>Cucumbers:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can be grown with beans, peas, and radishes, which help repel
+            cucumber beetles and other pests. They can also be grown with herbs
+            such as dill and chamomile, which attract beneficial insects such as
+            wasps and ladybugs.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="cell">
+          <p>Garlic and Onions:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Repel pests such as aphids, carrot flies, and cabbage worms. They
+            also improve the flavor of neighboring plants such as tomatoes and
+            peppers.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="cell">
+          <p>Green Beans:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can be grown with carrots and corn, which fix nitrogen in the soil.
+            They can also be grown with herbs such as rosemary and tarragon,
+            which repel pests such as bean beetles and aphids.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="cell">
+          <p>Lettuce:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can be grown with many plants as it is a great "filler" plant that
+            doesn't compete for resources. However, it can benefit from being
+            grown with plants that provide shade and help retain moisture, such
+            as taller plants like tomatoes.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="cell">
+          <p>Milkweed:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Attracts monarch butterflies and other beneficial insects. It is
+            also a host plant for milkweed bugs and beetles, which can help keep
+            pests away.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="cell">
+          <p>Potatoes:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can be grown with beans and peas, which fix nitrogen in the soil.
+            They can also be grown with marigolds, which repel pests such as
+            nematodes and wireworms.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="cell">
+          <p>Spicy Peppers:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can be grown with tomatoes, which help repel pests such as spider
+            mites and aphids. They can also be grown with herbs such as oregano
+            and thyme, which repel pests such as whiteflies and spider mites.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="cell">
+          <p>Spinach:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can also be grown with many plants as a filler plant. It can benefit
+            from being grown with plants that provide shade and help retain
+            moisture.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="cell">
+          <p>Summer Squash:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can be grown with radishes, which repel squash bugs and other pests.
+            They can also be grown with herbs such as dill and borage, which
+            attract beneficial insects such as bees and parasitic wasps.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="cell">
+          <p>Sweet Peas:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can be grown with carrots and other legumes, which fix nitrogen in
+            the soil. They can also be grown with herbs such as mint and thyme,
+            which attract beneficial insects such as bees and parasitic wasps.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="cell">
+          <p>Tomatoes:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can be grown with basil and other herbs, which improve their flavor
+            and repel pests such as hornworms and whiteflies. They can also be
+            grown with marigolds, which repel nematodes and other soil-borne
+            pests.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="cell">
+          <p>Winter Squash:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Can be grown with corn and beans, which provide shade and help
+            retain moisture. They can also be grown with marigolds and
+            nasturtiums, which repel pests such as squash bugs and cucumber
+            beetles.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="cell">
+          <p>Yarrow:</p>
+        </div>
+        <div class="cell">
+          <p>
+            Attracts beneficial insects such as ladybugs, lacewings, and
+            parasitic wasps that prey on garden pests. It also improves soil
+            quality by accumulating nutrients from deep in the ground.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+{
+  /* <p>Arugula: Can be grown with lettuce and other leafy greens, which can provide shade and help retain moisture. It can also be grown with herbs such as thyme and oregano, which repel pests such as flea beetles and aphids.</p>
 
 <p>Beets: Can be grown with garlic and onions, which repel pests such as leaf miners and aphids. They can also be grown with bush beans, which fix nitrogen in the soil.</p>
 
@@ -23,9 +277,5 @@ export default function CompanionPlanting() {
 <p>Sweet Peas: Can be grown with carrots and other legumes, which fix nitrogen in the soil. They can also be grown with herbs such as mint and thyme, which attract beneficial insects such as bees and parasitic wasps.</p>
 <p>Tomatoes: Can be grown with basil and other herbs, which improve their flavor and repel pests such as hornworms and whiteflies. They can also be grown with marigolds, which repel nematodes and other soil-borne pests.</p>
 <p>Winter Squash: Can be grown with corn and beans, which provide shade and help retain moisture. They can also be grown with marigolds and nasturtiums, which repel pests such as squash bugs and cucumber beetles.</p>
-<p>Yarrow: Attracts beneficial insects such as ladybugs, lacewings, and parasitic wasps that prey on garden pests. It also improves soil quality by accumulating nutrients from deep in the ground.</p>
-
-
-        </div>
-    )
+<p>Yarrow: Attracts beneficial insects such as ladybugs, lacewings, and parasitic wasps that prey on garden pests. It also improves soil quality by accumulating nutrients from deep in the ground.</p> */
 }
