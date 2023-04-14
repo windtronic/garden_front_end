0a88c4f7ad5bf44c1a74a5a46fedc0f5c6bbb2c4
Toni <windtronic89@gmail.com>
2023-04-14 16:14:47 -0600

    weather css is good enough fo me

--- garden_front_end/src/components/Weather.jsx
+++ garden_front_end/src/components/Weather.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Weather = () => {
   const [weatherData, setWeatherData] = useState(null);
@@ -9,7 +9,7 @@ const Weather = () => {
     const getData = async () => {
       try {
         const response = await axios.get(
-          `http://api.weatherapi.com/v1/forecast.json?key=639cf580b57b4d7da1300306231404&q=Arvada_Colorado&days=3`
+          `http://api.weatherapi.com/v1/forecast.json?key=639cf580b57b4d7da1300306231404&q=Arvada_Colorado&days=5`
         );
         setWeatherData(response.data);
       } catch (error) {
@@ -20,43 +20,49 @@ const Weather = () => {
     getData();
   }, []);
 
-  // Format the date string to "day of week name, month name day year"
-  const formatDate = dateString => {
+  const formatDate = (dateString) => {
     const date = new Date(dateString);
-    const options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' };
-    return date.toLocaleDateString('en-US', options);
+    const options = {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    };
+    return date.toLocaleDateString("en-US", options);
   };
 
-  // Get the current date and format it as YYYY-MM-DD
-  const currentDate = new Date().toISOString().split('T')[0];
+  const currentDate = new Date().toISOString().split("T")[0];
 
   return (
-    <div>
+    <div className="weather-container">
       {error ? (
         <p>Failed to retrieve weather data</p>
       ) : weatherData ? (
         <div>
-          <h2>2-day forecast for {weatherData.location.name}</h2>
+          <h2>4 Day Forecast For {weatherData.location.name}, CO</h2>
           {weatherData.forecast && weatherData.forecast.forecastday ? (
             weatherData.forecast.forecastday
-              // Filter out the first day in the forecast that matches the current date
-              .filter(day => day.date !== currentDate)
-              // Take the first three days in the filtered forecast array
-              .slice(0, 2)
-              .map(day => (
-                <div key={day.date}>
-                  <h3>{formatDate(day.date)}</h3>
-                  <p>High: {day.day.maxtemp_f}°F</p>
-                  <p>Low: {day.day.mintemp_f}°F</p>
-                  <p>Condition: {day.day.condition.text}</p>
-                </div>
+
+              .filter((day) => day.date !== currentDate)
+
+              .slice(0, 5)
+              .map((day) => (
+                <div key={day.date} className="weather-card">
+                <h3>{formatDate(day.date)}</h3>
+                <p><span className="high">High:</span> {day.day.maxtemp_f}°F</p>
+                <p><span className="low">Low:</span> {day.day.mintemp_f}°F</p>
+                <p><span className="condition">Condition:</span> {day.day.condition.text}</p>
+              </div>
+              
               ))
           ) : (
             <p>No forecast data available</p>
           )}
-          <h2>Current weather for {weatherData.location.name}</h2>
-          <p>Temperature: {weatherData.current.temp_f}°F</p>
-          <p>Condition: {weatherData.current.condition.text}</p>
+           <div className="current-weather">
+        <h2>Current Weather For {weatherData.location.name}, CO</h2>
+        <p>Temperature: {weatherData.current.temp_f}°F</p>
+        <p>Condition: {weatherData.current.condition.text}</p>
+      </div>
         </div>
       ) : (
         <p>Loading weather data...</p>
