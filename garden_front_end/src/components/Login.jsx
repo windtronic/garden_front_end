9f868d8f765250163250adfbec5107ff5c562075
Toni <windtronic89@gmail.com>
2023-06-04 13:20:12 -0600

    imported axios

--- garden_front_end/src/components/Login.jsx
+++ garden_front_end/src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Login() {
   const [email, setEmail] = useState('');
